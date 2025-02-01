e501a8909d219448c9f6f79008a2611c2cb21c4e
SuyogWaman1998 <suyogwaman2@gmail.com>
2025-02-01 11:59:34 +0530

    Comparator Demo with string buffer objects

--- src/main/java/Collections_Comparable_Comparator/Comparator_String_Stringbuffer.java
+++ src/main/java/Collections_Comparable_Comparator/Comparator_String_Stringbuffer.java
@@ -1,6 +1,7 @@
 package Collections_Comparable_Comparator;
 
-
+import java.util.Comparator;
+import java.util.TreeSet;
 
 /*
  * WAP to insert String and String buffer object in Treeset 
@@ -11,5 +12,34 @@ package Collections_Comparable_Comparator;
 
 public class Comparator_String_Stringbuffer {
 
+	public static void main(String[] args) {
+		TreeSet t = new TreeSet(new MyComparator());
+		
+		t.add("A");
+		t.add(new StringBuffer("ABC"));
+		t.add(new StringBuffer("AA"));
+		t.add("XX");
+		t.add("ABCD");
+		t.add("A");
+		t.add("X");
+		
+		System.out.println(t);
+		
+	}
+}
+
+class MyComparator implements Comparator{
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		String s1= o1.toString();
+		String s2 = o2.toString();
+		int l1= s1.length();
+		int l2 = s2.length();
+		
+		if(l1<l2) return -1;
+		else if(l1>l2) return +1;
+		else return s1.compareTo(s2);
+	}
 	
 }
